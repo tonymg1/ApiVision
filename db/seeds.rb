f5d901e05b12c3b39ff5b6065d0588152345f16b
tonymg1 <tonymartingaldeano@gmail.com>
2023-08-10 14:39:28 +0200

    [Config] data seed example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# user
+user = User.create(email: 'user@mail.com')
+
+# movies
+movie1 = Movie.create(title: 'Movie 1', plot: 'Plot of Movie 1')
+movie2 = Movie.create(title: 'Movie 2', plot: 'Plot of Movie 2')
+
+# seasons
+season1 = Season.create(title: 'Season 1', plot: 'Plot of Season 1', number: 1)
+season2 = Season.create(title: 'Season 2', plot: 'Plot of Season 2', number: 2)
+
+# episodes
+episode1 = Episode.create(episode_number: 1, season: season1)
+episode2 = Episode.create(episode_number: 2, season: season1)
+episode3 = Episode.create(episode_number: 1, season: season2)
+episode4 = Episode.create(episode_number: 2, season: season2)
+
+# purchase options
+purchase_option = PurchaseOption.create(purchase_price: 3.99, video_quality: 'HD', purchaseable: movie1)
+purchase_option2 = PurchaseOption.create(purchase_price: 2.99, video_quality: 'SD', purchaseable: season1)
+
+# library
+Library.create(user: user, purchase_option: purchase_option)
+
+
+puts "Data loaded succesfully"
